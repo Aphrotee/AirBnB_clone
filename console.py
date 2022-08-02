1833825705e3212c5139da67ea5dc164e86a4cb1
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-08-02 15:28:12 +0100

    updates

---
+++ console.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module contains the program for the entry point of the command interpreter
+"""
+
+
+class HBNBCommand(cmd.cmd):
+    """
+    This is the class that the command interpreter program
+    operates on.
+    """
+
+    
--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python3
 """
 This is the module that provides the base class
 BaseModel.
 """
 
 from datetime import datetime
-import json
+from models import storage
 import uuid
 
 
@@ -13,13 +14,22 @@ class BaseModel():
     attributes/methods for other classes.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         init function
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            self.id = kwargs["id"]
+            self.my_number = kwargs["my_number"]
+            self.name = kwargs["name"]
+            self.created_at = datetime.fromisoformat(str(kwargs["created_at"]))
+            self.updated_at = datetime.fromisoformat(str(kwargs["updated_at"]))
+        else:
+            storage.new(self)
+        print(self.__dict__)
 
     def __str__(self):
         """
@@ -32,15 +42,17 @@ class BaseModel():
         Updates the public instance attribute
         updated_at with the current datetime
         """
-        self.updated_at = datetime.now()
+        self.updated_at = str(datetime.now())
+        storage.save()
 
     def to_dict(self):
         """
         Returns a dictionary containing all
         keys/values of __dict__ of the instance
         """
-        obj_dict = self.__dict__
+        obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = type(self).__name__
         obj_dict['created_at'] = str(obj_dict['created_at'].isoformat())
         obj_dict['updated_at'] = str(obj_dict['updated_at'].isoformat())
         return obj_dict
+
